e87ffe2da13a1df0b98ca74aefce2c2addded153
Amin-pr <127015189+Amin-pr@users.noreply.github.com>
2023-12-31 20:16:46 +0330

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,54 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+//need to impliment routes this way for data fetching
+const router = createBrowserRouter([
+  {
+    //Layout route
+    element: <AppLayout />,
+    //we can use error here to replace whole app
+    // errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        errorElement: <Error />,
+
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        //or we can use error here to replace only this component
+        errorElement: <Error />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+        errorElement: <Error />,
+      },
+
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        errorElement: <Error />,
+      },
+      {
+        path: "/order/ordeId",
+        element: <Order />,
+        errorElement: <Error />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  const x = 23;
-  return (
-    <div>
-      <p>heloo</p>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
